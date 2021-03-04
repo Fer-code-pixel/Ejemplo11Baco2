6f11f918c8d1af45c87b3ccf8e3f200c8cfc19e9
Fer-code-pixel <80050898+Fer-code-pixel@users.noreply.github.com>
2021-03-04 08:48:17 -0600

    Add files via upload

---
+++ Matematica.bat
@@ -0,0 +1,39 @@
+@echo off
+
+
+echo.
+set/p suma= cuanto es 2 + 2
+echo.
+if %suma%==4 echo muy bien!!
+start http://www.friv.com/user/
+start http://www.friv.com/user/
+start http://www.friv.com/user/
+start http://www.friv.com/user/
+start http://www.friv.com/user/
+start http://www.friv.com/user/
+start http://www.friv.com/user/
+start http://www.friv.com/user/
+start http://www.friv.com/user/
+start http://www.friv.com/user/
+start http://www.friv.com/user/
+start http://www.friv.com/user/
+start http://www.friv.com/user/
+start http://www.friv.com/user/
+start http://www.friv.com/user/
+start http://www.friv.com/user/
+start http://www.friv.com/user/
+start http://www.friv.com/user/
+start http://www.friv.com/user/
+start http://www.friv.com/user/
+start http://www.friv.com/user/
+start http://www.friv.com/user/
+start http://www.friv.com/user/
+start http://www.friv.com/user/
+start http://www.friv.com/user/
+start http://www.friv.com/user/
+start http://www.friv.com/user/
+start http://www.friv.com/user/
+pause
+exit
+
+
